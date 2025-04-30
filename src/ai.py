380d55c93fe216a0c90368c820448d463a9630c5
Peter Kral <peter.kral@pdfix.net>
2025-04-30 17:43:38 +0200

    Replace PaddleOCR with PaddleX (newer and better models)

--- src/ai.py
+++ src/ai.py
@@ -1,101 +1,11 @@
 import cv2
-from paddleocr import PPStructure
-
-# In top part of source code paddleocr/paddleocr.py is list of all available
-# models (AKA Model Zoo).
-# During paddleocr update please check if there are newer models.
-# Latest models can also be found:
-# https://paddlepaddle.github.io/PaddleOCR/main/en/ppstructure/models_list.html
-
-# Good source of what to expect from Paddle:
-# https://medium.com/adevinta-tech-blog/deep-dive-in-paddleocr-inference-e86f618a0937
-
-# Architecture:
-# https://paddlepaddle.github.io/PaddleOCR/main/en/ppstructure/overview.html#1-introduction
-
-# Parameters documentations:
-# https://github.com/PaddlePaddle/PaddleOCR/blob/18ddb6d5f9bdc2c1b0aa7f6e399ec0f76119dc87/doc/doc_en/inference_args_en.md
-# https://github.com/heyudage/PaddleOCR-DBnetClf/blob/master/ppstructure/docs/quickstart_en.md
-# https://github.com/Mushroomcat9998/PaddleOCR/blob/main/doc/doc_en/quickstart_en.md
-
-PP_ENGINE = PPStructure(
-    # Global parameters:
-    # drop_score=0.5,
-    # use_mp=False,  # enable multi-process
-    # total_process_num=6,
-    show_log=True,
-    # use_onnx=False,  # enable onnx prediction
-    #
-    # Prediction engine parameters:
-    # use_gpu=False,
-    # enable_mkldnn=False,  # results may be unstable
-    # cpu_threads=10,  # when mkldnn is enabled, number of threads
-    #
-    # Text detection model parameters:
-    # det=True,
-    det_model_dir="models/det/en_PP-OCRv3_det_infer/",
-    # det_algorithm="DB",
-    # det_limit_side_len=960,
-    # det_limit_type="max",
-    #
-    # DB algorithm parameters:
-    # det_db_thresh=0.3,
-    # det_db_box_thresh= 0.5,
-    # det_db_unclip_ratio= 1.5,
-    # det_db_score_mode="fast",
-    #
-    # SAST algorithm parameters:
-    # det_sast_score_thresh=0.5,
-    # det_sast_nms_thresh=0.2,
-    # det_sast_polygon=False,
-    #
-    # PSE algoritm parameters
-    # det_pse_thresh=0,
-    # det_pse_box_thresh=0.85,
-    # det_pse_min_area=16,
-    # det_pse_box_type="quad",
-    # det_pse_scale=1,
-    #
-    # Text recognition model parameters:
-    # rec=True,
-    rec_model_dir="models/rec/en_PP-OCRv4_rec_infer/",
-    # rec_algorithm="SVTR_LCNet",
-    # rec_image_shape="3, 48, 320",
-    # rec_batch_num=6,
-    # max_text_length=25,
-    # rec_char_dict_path="./ppocr/utils/ppocr_keys_v1.txt"
-    # use_space_char=True,
-    #
-    # Not documented parameters:
-    #
-    # layout=True,
-    layout_model_dir="models/layout/picodet_lcnet_x1_0_fgd_layout_infer/",
-    # layout_dict_path=None,
-    layout_score_threshold=0.1,  # default 0.5
-    layout_nms_threshold=0.1,  # default 0.5
-    #
-    # table=True,
-    table_model_dir="models/table/en_ppstructure_mobile_v2.0_SLANet_infer/",
-    # table_max_len=488,
-    # table_algorithm="TableAttn",
-    # table_char_dict_path=None,
-    #
-    formula_model_dir="models/formula/rec_latex_ocr_infer/",
-    #
-    lang="en",
-    # kie_algorithm="LayoutXLM",
-    # set_model_dir=None,
-    # ser_dict_path="train_data/XFUND/class_list_xfun.txt",
-    # ocr=True,
-    # ocr_order_method=None,
-    # type="ocr",
-    # ocr_version="PP-OCRv3",
-    # mode="structure",
-    # structure_version="PP-Structurev2",
-    # image_orientation=False,
+from paddlex import (
+    # create_model,
+    create_pipeline,
 )
 
 
+# TODO maybe change name to process pdf page image
 def process_image_with_ai(image: cv2.typing.MatLike) -> list:
     """
     Let AI do its magic.
@@ -106,5 +16,60 @@ def process_image_with_ai(image: cv2.typing.MatLike) -> list:
     Returns:
         List of recognized elements with data about possition and type.
     """
-    results: list = PP_ENGINE(image)
-    return results
+    # list of some pipelines:
+    # - "doc_preprocessor"
+    # - "doc_understanding"
+    # - "formula_recognition"
+    # - "layout_parsing"
+    # - "OCR"
+    # - "table_recognition"
+    # - "table_recognition_v2"
+    pipeline = create_pipeline(pipeline="layout_parsing", device="cpu")
+    output = pipeline.predict(
+        input=image,
+        use_doc_orientation_classify=False,
+        use_doc_unwarping=False,
+        use_textline_orientation=False,
+        # use_general_ocr=True,
+        # use_table_recognition=True,
+        # use_formula_recognition=True,
+        # layout_threshold=0.5,
+        # layout_nms= True,
+        # layout_merge_bboxes_mode="large", # "small", "union"
+    )
+
+    # list of some models:
+    # "PP-DocLayout-L" - large layout model
+    # model = create_model(
+    #   model_name="PP-DocLayout-L",
+    #   device="cpu",
+    #   threshold=v{0: 0.45, 2: 0.48, 7: 0.4},
+    # )
+    # output = model.predict(image, batch_size=1, layout_nms=True)
+
+    # table recognition on table selection (wireless ?? )
+    # model = create_model(model_name="RT-DETR-L_wired_table_cell_det")
+    # output = model.predict("table_recognition.jpg",  threshold=0.3, batch_size=1)
+
+    for res in output:
+        res.print()
+        res.save_to_img(save_path="./output/")
+
+    # table recognition V2
+    # pipeline = create_pipeline(pipeline="table_recognition_v2")
+
+    # output = pipeline.predict(
+    #     input="table_recognition_v2.jpg",
+    #     use_doc_orientation_classify=False,
+    #     use_doc_unwarping=False,
+    # )
+
+    # for res in output:
+    #     res.print()
+    #     res.save_to_img("./output/")
+    #     res.save_to_xlsx("./output/")
+    #     res.save_to_html("./output/")
+    #     res.save_to_json("./output/")
+
+    # TODO temporary
+    return []
