db30f033504ec4ac1532915f7fad4a12e04f481d
Peter Kral <peter.kral@pdfix.net>
2025-06-25 17:22:11 +0200

    PVQ-3923 External action - add simple test to all - adding simple test to test if there are any issues to build and push github action with testing

---
+++ test.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# This is local docker test during build and push action.
+
+# Colors for output into console
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
+
+# Function to print info messages
+info() { echo -e "${PURPLE}$1${NC}"; }
+
+# Function to print success messages
+success() { echo -e "${GREEN}$1${NC}"; }
+
+# Function to print error messages
+error() { echo -e "${RED}ERROR: $1${NC}"; }
+
+# init
+pushd "$(dirname $0)" > /dev/null
+
+EXIT_STATUS=0
+DOCKER_IMAGE="pdf-accessibility-paddle:test"
+PLATFORM="--platform linux/amd64"
+TEMPORARY_DIRECTORY=".test"
+
+info "Building docker image..."
+docker build $PLATFORM --rm -t $DOCKER_IMAGE .
+
+if [ -d "$(pwd)/$TEMPORARY_DIRECTORY" ]; then
+    rm -rf $(pwd)/$TEMPORARY_DIRECTORY
+fi
+mkdir -p $(pwd)/$TEMPORARY_DIRECTORY
+
+info "List files in cwd"
+docker run --rm $PLATFORM -v $(pwd):/data -w /data --entrypoint ls $DOCKER_IMAGE
+
+info "Test #01: Show help"
+docker run --rm $PLATFORM -v $(pwd):/data -w /data $DOCKER_IMAGE --help > /dev/null
+if [ $? -eq 0 ]; then
+    success "passed"
+else
+    error "Failed to run \"--help\" command"
+    EXIT_STATUS=1
+fi
+
+info "Test #02: Extract config"
+docker run --rm $PLATFORM -v $(pwd):/data -w /data $DOCKER_IMAGE config -o $TEMPORARY_DIRECTORY/config.json > /dev/null
+if [ -f "$(pwd)/$TEMPORARY_DIRECTORY/config.json" ]; then
+    success "passed"
+else
+    error "config.json not saved"
+    EXIT_STATUS=1
+fi
+
+info "Test #03: Run simple autotag with default parameters"
+docker run --rm $PLATFORM -v $(pwd):/data -w /data $DOCKER_IMAGE tag -i example/air_quality.pdf -o $TEMPORARY_DIRECTORY/air_quality.pdf > /dev/null
+if [ -f "$(pwd)/$TEMPORARY_DIRECTORY/air_quality.pdf" ]; then
+    success "passed"
+else
+    error "simple autotag failed on example/air_quality.pdf"
+    EXIT_STATUS=1
+fi
+
+info "Removing testing docker image"
+docker rmi $DOCKER_IMAGE
+
+popd > /dev/null
+
+if [ $EXIT_STATUS -eq 1 ]; then
+    error "One or more tests failed."
+    exit 1
+else
+    success "All tests passed."
+    exit 0
+fi
