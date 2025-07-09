0a54ae68ac8007658644fff763df9fa0a6458276
Peter Kral <peter.kral@pdfix.net>
2025-07-09 17:23:39 +0200

    Docker testing build image without cache. - github action workflow using new Dockerfile.withoutcache + disabled FTP uploads - added Dockerfile.withoutcache that combines Dockerfile and Dockerfile.cache - increased Dockerfile image version it uses (should be multiplatform but it is not visible in docker hub yet) - made build_cached_image.sh just print warning as it is WIP

--- build_cached_image.sh
+++ build_cached_image.sh
@@ -2,12 +2,30 @@
 # Then this script needs to be run manually
 # Then push git commit to GitHub and tag it so finall image is built and pushed to Docker hub
 
+echo "This script is currently WIP and are just notes with commands."
+exit 0
+
+# -------- JUST FOR ONE PLATFORM ------
 
 # Change version manually as this will be run very rarely
 docker build -f Dockerfile.cache -t pdfix/pdf-accessibility-paddle-cache:v0.0.1 .
+docker build --platform linux/amd64 -f Dockerfile.cache -t pdfix/pdf-accessibility-paddle-cache:v0.0.1 .
 
 
 # Push to Docker Hub
 # For this step you need to be logged in to Docker Hub
 # You can log in using `docker login`
 docker push pdfix/pdf-accessibility-paddle-cache:v0.0.1
+
+# -------- 2 OR MORE PLATFORMS ------
+
+# check for
+# Driver: docker-container
+# Platforms: linux/amd64, linux/arm64, ...
+
+# create builder:
+docker buildx create --use --name multiarch-builder --driver docker-container
+docker buildx inspect --bootstrap
+
+# build and push
+docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile.cache -t pdfix/pdf-accessibility-paddle-cache:v0.0.2 --push .
