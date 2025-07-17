e1026f8d759172d225c88e4b16483efbce392ca3
Peter Kral <peter.kral@pdfix.net>
2025-07-17 16:02:54 +0200

    PVQ-4115 External Actions - docker build in test.sh remove --rm

--- test.sh
+++ test.sh
@@ -26,7 +26,7 @@ PLATFORM="--platform linux/amd64"
 TEMPORARY_DIRECTORY=".test"
 
 info "Building docker image..."
-docker build -f Dockerfile $PLATFORM --rm -t $DOCKER_IMAGE .
+docker build -f Dockerfile $PLATFORM -t $DOCKER_IMAGE .
 
 if [ -d "$(pwd)/$TEMPORARY_DIRECTORY" ]; then
     rm -rf $(pwd)/$TEMPORARY_DIRECTORY
