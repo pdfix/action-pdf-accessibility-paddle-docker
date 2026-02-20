030782ddc31a8d94a37c4483ef409e52833b85b0
Peter Kral <peter.kral@pdfix.net>
2026-02-20 17:22:47 +0100

    fix(action,PVQ-4872): Chosen different docker API (that works and returns data) to retrieve latest tag.

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,7 @@
 CONFIG_FILE = "config.json"
-DOCKER_IMAGE = "pdfix/pdf-accessibility-paddle"
+DOCKER_NAMESPACE = "pdfix"
+DOCKER_REPOSITORY = "pdf-accessibility-paddle"
+DOCKER_IMAGE = f"{DOCKER_NAMESPACE}/{DOCKER_REPOSITORY}"
 MATH_ML_VERSION = "mathml-3"
 IMAGE_FILE_EXT_REGEX = r"\.(jpg|jpeg|png|bmp)$"
 SUPPORTED_IMAGE_EXT = ".jpg .jpeg .png .bmp"
